2b6960c108561d80a1762a9c32bdc26c40326979
kuman-lenovo <775121173@qq.com>
2018-03-24 00:51:38 +0800

    add file

---
+++ example/pipelines.py
@@ -0,0 +1,26 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/topics/item-pipeline.html
+from datetime import datetime
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+
+class ExamplePipeline(object):
+    def process_item(self, item, spider):
+        item["crawled"] = datetime.utcnow()
+        item["spider"] = spider.name
+        return item
+
+class MyImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+          for image_url in item['image_urls']:
+              yield Request(image_url)
+              
+    def item_completed(self, results, item, info):
+         image_path = [x['path'] for ok,x in results if ok]
+         if not image_path:
+             raise DropItem('Item contains no images')
+         item['image_paths'] = image_path
+         return item
---
+++ example/spiders/dmoz.py
@@ -0,0 +1,27 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spider import Rule
+from scrapy_redis.spiders import CrawlSpider
+from example.items import baiduTieba
+
+class DmozSpider(CrawlSpider):
+    """Follow categories and extract links."""
+    name = 'dmoz'
+    #allowed_domains = ['dmoz.org']
+    start_urls = ['http://tieba.baidu.com/p/3707205431']
+    img_urls = []
+    
+    rules = [
+        Rule(LinkExtractor(allow=(r"/p/3707205431\?pn")), callback='parse_directory', follow=True),
+    ]
+
+    def parse_directory(self, response):
+        items1 = baiduTieba()
+        items = baiduTieba()
+        items['title'] = "".join(response.xpath("//title/text()").extract())
+        x1 = response.xpath("//img[@class='BDE_Image']/@src").extract()
+        items['images_urls'] = "".join(x1)
+        for x in x1:
+            #print(x)
+            yield {"images_urls":x}
+        yield items
+
